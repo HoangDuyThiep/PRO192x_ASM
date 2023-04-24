60bc30d209c3ec9b646fedb7ace126b4038ebd2a
thiep <hoangduythiep@gmail.com>
2023-04-24 23:01:24 +0900

    24/4/23 thấy khá ổn

--- vn.funix.FX21316.java/src/vn/funix/FX21316/java/asm01/Asm01.java
+++ vn.funix.FX21316.java/src/vn/funix/FX21316/java/asm01/Asm01.java
@@ -4,9 +4,11 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Asm01 {
-    public static void main(String[] args) {
 
-
+    //hiện thị mở đầu
+    static void hienThiDau() {
+        System.out.println("|  Chương trình kiểm tra thông tin cá nhân thông  |");
+        System.out.println("|  qua số căn cước công dân                       |");
         System.out.println("+----------+---------------------------+----------+");
         System.out.println("|  THÔNG TIN SỐ  | Hoàng Duy Thiệp FX21316@V1.0.0 |");
         System.out.println("+----------+---------------------------+----------+");
@@ -14,51 +16,113 @@ public class Asm01 {
         System.out.println("|  0. Thoát                                       |");
         System.out.println("+----------+---------------------------+----------+");
         System.out.println("Chức năng: ");
+    }
 
 
+    //tạo mã số xác thực
+    static int maXacThuc() {
+        Random rd = new Random();
+        int newRd = 100 + rd.nextInt(899);
+        return newRd;
+    }
+    //hiện thị mã xác thục cho người dùng
+    static void hienThiMXT(int newRd) {
+        System.out.println("+----------+---------------------------+----------+");
+        System.out.println("Mã số xác thực là: " +  newRd);
+        System.out.println("|  0. Thoát                                       |");
+        System.out.println("+----------+---------------------------+----------+");
+        System.out.println("Mời bạn nhập mã số xác thực: ");
+    }
+    //hiển thị khi mã xác thực chính xác
+    static void chinhXacMXT() {
+        System.out.println("+----------+----Mã xác thực chính xác -+----------+");
+        System.out.println("|  0. Thoát                                       |");
+        System.out.println("+----------+---------------------------+----------+");
+        System.out.println("Mời nhập căn cước công dân: ");
+    }
+    static void loiChucNang() {
+        System.out.println("| Lỗi nhập lựa chọn chức năng Mentor ạ");
+        System.out.println("| Mentor nhập 0 hoặc 1 thôi ạ, em cô đơn quá! T_T");
 
+    }
+    static void hienThiChucNang() {
+        System.out.println("+----------+----Số căn cước hợp lệ-----+----------+");
+        System.out.println("| 1. Kiểm tra nơi sinh");
+        System.out.println("| 2. Kiểm tra tuôi, giới tính");
+        System.out.println("| 3. Kiểm tra số ngẫu nhiên");
+        System.out.println("| 0. thoát");
+        System.out.println("+----------+---------------------------+----------+");
+        System.out.println("Chức năng: ");
+    }
+    //-------------------------------------------main----------------------------
+    public static void main(String[] args) {
+
+        hienThiDau();
 
         Scanner sc = new Scanner(System.in);
+        int chucNang;
+        //xử lý khi nhập sai phím chức năng
+        try {
+            chucNang = sc.nextInt();
+        }
+        catch (Exception ex) {
+            loiChucNang();
+            chucNang = 2;
+        }
+//        sc.nextLine();
 
-        int chucNang = sc.nextInt();
+        //kiểm tra phím chức năng của lựa chọn đầu
         while (true) {
-
-            if (chucNang == 0 || chucNang == 1)
+            if (chucNang == 0 || chucNang == 1) {
                 break;
+            }
             System.out.println("+----------+---------------------------+----------+");
             System.out.println("Mời nhập lại phím chức năng: ");
             System.out.println("+----------+---------------------------+----------+");
-            chucNang = sc.nextInt();
+            try {
+                sc.nextLine();
+                chucNang = sc.nextInt();
+            }
+            catch (Exception ex) {
+                loiChucNang();
+                chucNang = 2;
+
+            }
+
         }
+
         if (chucNang == 1) {
-            Random rd = new Random();
-            int newRd = 100 + rd.nextInt(899);
-            System.out.println("+----------+---------------------------+----------+");
-            System.out.println("Mã số xác thực là: " +  newRd);
-            System.out.println("|  0. Thoát                                       |");
-            System.out.println("+----------+---------------------------+----------+");
-            System.out.println("Mời bạn nhập mã số xác thực: ");
+            //tạo mã xác thực
+            int newRd = maXacThuc();
+            hienThiMXT(newRd);
 
             //kiểm tra số xác thực và thực hiện lựa chọn
             int xacThuc = -1;
             while (xacThuc != 0) {
-                xacThuc = sc.nextInt();
+                //xử lý khi gặp lỗi mã xác thực
+                try {
+                    xacThuc = sc.nextInt();
+                }
+                catch (Exception ex) {
+                    System.out.println("| Em biết ngay Mentor cố tình nhập sai mà!");
+                    System.out.println("| Mentor xem lại "+ newRd + " giúp em ạ! T_T");
+                    System.out.println("| Em chưa có bạn gái mà cày code thế này chắc ế tới chớt! T_T");
+                    xacThuc = 2;
+                }
+
                 sc.nextLine();
+
 
                 //khi mã xác thực chính xác
                 if (xacThuc == newRd) {
-                    System.out.println("+----------+----Mã xác thực chính xác -+----------+");
-                    System.out.println("|  0. Thoát                                       |");
-                    System.out.println("+----------+---------------------------+----------+");
-                    System.out.println("Mời nhập căn cước công dân: ");
+                    chinhXacMXT();
                     String soCanCuoc = sc.nextLine();
                     Cccd cccd1 = new Cccd(soCanCuoc);
 
 
                     //vòng lặp nhập cccd
                     while (!soCanCuoc.equals("0")) {
-
-
+                        //kiểm tra căn cước công dân hợp lệ
                         while (!cccd1.checkcccd() || !cccd1.checkMaTinh() || !cccd1.checkGioiTinh()){
                             System.out.println("Số căn cước công dân không hợp lệ, vui lòng nhập lại");
                             System.out.println("| 0. thoát");
@@ -75,13 +139,7 @@ public class Asm01 {
                         if (cccd1.checkcccd() && cccd1.checkMaTinh() && cccd1.checkGioiTinh()) {
                             int luaChon = -1;
                             while (luaChon != 0) {
-                                System.out.println("+----------+----Số căn cước hợp lệ-----+----------+");
-                                System.out.println("| 1. Kiểm tra nơi sinh");
-                                System.out.println("| 2. Kiểm tra tuôi, giới tính");
-                                System.out.println("| 3. Kiểm tra số ngẫu nhiên");
-                                System.out.println("| 0. thoát");
-                                System.out.println("+----------+---------------------------+----------+");
-                                System.out.println("Chức năng: ");
+                                hienThiChucNang();
                                 luaChon = sc.nextInt();
 
                                 //các lựa chon chức năng
